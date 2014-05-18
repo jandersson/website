9cd33585c55904b4dbcf5b9fc35b6ddf9d6637de
Jonas Andersson <Jonas123@gmail.com>
2014-05-18 18:24:23 -0500

    complete asciichan project

--- main.py
+++ main.py
@@ -165,20 +165,31 @@ class MainPage(webapp2.RequestHandler):
     def post(self):
     	self.response.out.write("Thanks! That's a totally valid day!")
 
+class Art(db.Model):
+	title = db.StringProperty(required = True)
+	art = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+
 class AsciiChandler(Handler):
 	def render_front(self, title="", art="", error=""):
-		self.render("asciichan.html", title=title, art=art, error=error)
+		arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
+		self.render("asciichan.html", title=title, art=art, error=error, arts=arts)
+		
 	def get(self):
 		self.render_front()
+		
 	def post(self):
 		title = self.request.get("title")
 		art = self.request.get("art")
 
 		if title and art:
-			self.write("thanks!")
+			a = Art(title = title, art = art)
+			a.put()
+			self.redirect("/asciichan")
 		else:
 			error = "You need to fill out both fields"
 			self.render_front(title=title, art=art, error=error)
+
 class FizzBuzz(Handler):
 	def get(self):
 		n = self.request.get("n", 0)
